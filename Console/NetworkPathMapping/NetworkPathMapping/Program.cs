1a86ec82c8e973de9218c3b1325a847aea85b0ea
Thomas Stekovits <t.stekovits@hotmail.com>
2022-10-13 09:47:21 +0200

    Add files via upload

---
+++ Console/NetworkPathMapping/NetworkPathMapping/Program.cs
@@ -0,0 +1,34 @@
+﻿using NetworkPathMapping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkPathMapping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (!DriveSettings.IsDriveMapped("L"))
+                {
+                    Console.WriteLine("");
+                    DriveSettings.MapNetworkDrive("L", @"\\%ComputerName%\%ShareName%");
+                }
+                else
+                {
+                    DriveSettings.DisconnectNetworkDrive("L", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error during network path un/mapping action!\n{ex.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
