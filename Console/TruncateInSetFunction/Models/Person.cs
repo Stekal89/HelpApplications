6cffd17187cd8eed6e222e7722f4477a15f932b8
Thomas Stekovits <t.stekovits@hotmail.com>
2023-01-24 14:35:45 +0100

    Truncate inside of Property-Set
    Example of how to truncate text and how to implement usability inside of Set-Method of Property.

---
+++ Console/TruncateInSetFunction/Models/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruncateInSetFunction.Models
+{
+    public class Person
+    {
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set 
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    firstName = Program.TruncateText(value, 4);
+                }
+                else
+                {
+                    firstName = value;
+                }
+            }
+        }
+
+
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Shows the infromation of the Properties
+        /// </summary>
+        public void ShowInformation()
+        {
+            Console.WriteLine($"---------------------------------");
+            Console.WriteLine($"--- Person ---");
+            Console.WriteLine($"FirstName: \"{FirstName}\"");
+            Console.WriteLine($"Length: \"{FirstName.Length}\"");
+            Console.WriteLine($"FirstName: \"{LastName}\"");
+            Console.WriteLine($"Length: \"{LastName.Length}\"");
+            Console.WriteLine($"---------------------------------\n");
+        }
+    }
+}
---
+++ Console/TruncateInSetFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TruncateInSetFunction.Models;
+
+namespace TruncateInSetFunction
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example is inside the Set-Functionality of the Person class
+
+            Person person = new Person()
+            {
+                FirstName = "Markus",
+                LastName = "Thomson"
+            };
+
+            person.ShowInformation();
+
+
+            Console.WriteLine("\n\nContinue with any key...");
+            Console.ReadKey();
+        }
+
+        public static string TruncateText(this string strValue, int nMaxLength)
+        {
+            if (string.IsNullOrEmpty(strValue)) return strValue;
+            return strValue.Length <= nMaxLength ? strValue : strValue.Substring(0, nMaxLength);
+        }
+    }
+}
