a2acc3e63555e976a368ef5d9f7a7f863fd846c6
Thomas Stekovits <t.stekovits@hotmail.com>
2023-06-15 06:52:01 +0200

    Extended Round Project with one Custom Function
    Round Digits from right to left, not only the next digit to the wished.

--- Console/Round/Program.cs
+++ Console/Round/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Round
 {
@@ -21,7 +18,8 @@ namespace Round
                 {
 					if (decimal.TryParse(strInput, out decimal nInputToRound))
 					{
-						decimal nResult = Math.Round(nInputToRound, MidpointRounding.AwayFromZero);
+                        //decimal nResult = Math.Round(nInputToRound, 2, MidpointRounding.AwayFromZero);
+                        decimal nResult = RoundNumber(nInputToRound, 2);
 
                         Console.WriteLine($"Result: {nResult}");
 					}
@@ -39,7 +37,40 @@ namespace Round
 				Console.ReadKey();
 				Console.Clear();
 
-			} while (strInput.ToUpper() != "E" && strInput.ToUpper() != "EXIT");
+			} while (strInput.ToUpper() != "E" && strInput.ToUpper() != "EXIT");    
         }
-	}
+
+        /// <summary>
+        /// Round for each digit from right to left
+        /// </summary>
+        /// <param name="number">Number which should be round</param>
+        /// <param name="decimals">digits after the comma</param>
+        /// <returns>Rounded Number</returns>
+        public static decimal RoundNumber(decimal number, int decimals)
+        {
+            decimal result = number;
+
+            if (Math.Truncate(number) != number)
+            {
+                int commaCount = BitConverter.GetBytes(decimal.GetBits(number)[3])[2];
+                int count = number.ToString().Count() - 1;
+
+                int commaCountDecimals =  commaCount - decimals;
+                int diff = count - commaCount;
+
+                if (commaCountDecimals > 0 && diff > 0)
+                {
+                    result = number;
+
+                    for (int i = 0; i < commaCountDecimals; i++)
+                    {
+                        result = Math.Round(result, commaCount - 1, MidpointRounding.AwayFromZero);
+                        commaCount--;
+                        Console.WriteLine(result);
+                    }
+                }
+            }
+            return result;
+        }
+    }
 }
