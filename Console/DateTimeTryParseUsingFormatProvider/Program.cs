f17135b6243a43cd610f62fac7fee26144add1c3
Thomas Stekovits <t.stekovits@hotmail.com>
2023-09-14 07:32:51 +0200

    Parse string to DateTime using IFormatProvider

---
+++ Console/DateTimeTryParseUsingFormatProvider/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DateTimeTryParseUsingFormatProvider
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			string dateString = "2023-08-29";
+			DateTime date;
+
+			// create IFormatProvider
+			//IFormatProvider formatProvider = new System.Globalization.CultureInfo("en-US");
+			//IFormatProvider formatProvider = new System.Globalization.CultureInfo("de-DE");
+
+			// DateTime.TryParse using IFormatProvider
+			//if (DateTime.TryParse(dateString, formatProvider, System.Globalization.DateTimeStyles.None, out date))
+			if (DateTime.TryParse(dateString, new System.Globalization.CultureInfo("de-DE"), System.Globalization.DateTimeStyles.None, out date))
+			{
+				Console.WriteLine("Date was successfully parsed: " + date.ToString("yyyy-MM-dd"));
+			}
+			else
+			{
+				Console.WriteLine("It was not possible to parse the date.");
+			}
+
+			Console.WriteLine("\n\nContinue with any key...");
+			Console.ReadKey();
+		}
+	}
+}
