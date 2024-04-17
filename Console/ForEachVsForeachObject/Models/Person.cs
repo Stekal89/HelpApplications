afd7e33982e809e48f65e3e750c1239f8ea54284
Thomas Stekovits <t.stekovits@hotmail.com>
2024-04-17 11:50:55 +0200

    Example of using the ForeEach Object Function
    Example of using the ForeEach functionality, which is included in Generic Lists.
    3 Examples:
    - Filled List
    - Empty List
    - Null List

---
+++ Console/ForEachVsForeachObject/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEachVsForeachObject.Models
+{
+	public class Person
+	{
+        /// <summary>
+        /// Full-Name
+        /// </summary>
+        public string Name { get; set; } = String.Empty;
+
+        /// <summary>
+        /// Age
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Default-Constructor
+        /// </summary>
+        public Person()
+        {
+                
+        }
+
+        /// <summary>
+        /// Full Constructor
+        /// </summary>
+        /// <param name="name">Name of Person</param>
+        /// <param name="age">Age of Person</param>
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Console/ForEachVsForeachObject/Program.cs
@@ -0,0 +1,112 @@
+﻿using ForEachVsForeachObject.Models;
+using System.Threading.Channels;
+
+namespace ForEachVsForeachObject
+{
+	internal class Program
+	{
+		static Random random = new Random();
+
+		static void Main(string[] args)
+		{
+			#region Filled List
+
+			List<Person> people = new List<Person>()
+			{
+				new Person("Jonny Jonson", 23),
+				new Person("Jimmy Jackson", 32),
+				new Person("Angela Smith", 25)
+			};
+
+			// Normal Implementation:
+			Console.WriteLine("Filled List (Normal Implementation):");
+			ModifyAgesInForEach(people);
+
+			// ForEach Object Function
+			Console.WriteLine("Filled List (ForEach Object Function):");
+			Console.WriteLine("\n---------------------------------------");
+			people.ForEach(person =>
+			{
+				int newAge = random.Next(5, 40);
+
+				Console.WriteLine($"Change Age of \"{person.Name}\" from \"{person.Age}\" to \"{newAge}\"");
+
+				person.Age = newAge;
+			});
+			Console.WriteLine("---------------------------------------\n");
+
+			#endregion // Filled List
+
+			#region Empty List
+
+			people = new List<Person>();
+
+			// Normal Implementation:
+			Console.WriteLine("Empty List (Normal Implementation):");
+			ModifyAgesInForEach(people);
+
+			// ForEach Object
+			Console.WriteLine("Empty List (ForEach Object Function):");
+			Console.WriteLine("\n---------------------------------------");
+			people?.ForEach(person =>
+			{
+				int newAge = random.Next(5, 40);
+
+				Console.WriteLine($"Change Age of \"{person.Name}\" from \"{person.Age}\" to \"{newAge}\"");
+
+				person.Age = newAge;
+			});
+			Console.WriteLine("---------------------------------------\n");
+
+			#endregion // Empty List
+
+
+			#region Null List
+
+			people = null;
+
+			// Normal Implementation:
+			Console.WriteLine("Null List (Normal Implementation):");
+			ModifyAgesInForEach(people);
+
+			// ForEach Object
+			Console.WriteLine("NULL List (ForEach Object Function):");
+			Console.WriteLine("\n---------------------------------------");
+
+			// ####################
+			// Using the QuestionMark, makes it possible to use the ForEach Object Functionality at this way!!!
+			// ####################
+			people?.ForEach(person =>
+			{
+				int newAge = random.Next(5, 40);
+
+				Console.WriteLine($"Change Age of \"{person.Name}\" from \"{person.Age}\" to \"{newAge}\"");
+
+				person.Age = newAge;
+			});
+			Console.WriteLine("---------------------------------------\n");
+
+			#endregion // Null List
+
+			Console.WriteLine($"\n\nContinue with any key...");
+			Console.ReadKey();
+        }
+
+		private static void ModifyAgesInForEach(List<Person> people)
+		{
+			if (people != null && people.Count > 0)
+			{
+                Console.WriteLine("\n---------------------------------------");
+                foreach (var item in people)
+				{
+					int newAge = random.Next(5, 40);
+
+                    Console.WriteLine($"Change Age of \"{item.Name}\" from \"{item.Age}\" to \"{newAge}\"");
+
+                    item.Age = newAge;
+				}
+				Console.WriteLine("---------------------------------------\n");
+			}
+		}
+	}
+}
