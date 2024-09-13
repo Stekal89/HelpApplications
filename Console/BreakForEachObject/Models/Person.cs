18dd2176161ab1cffa793e7fd37aeddfdcdc2405
Thomas Stekovits <t.stekovits@hotmail.com>
2024-09-13 07:36:29 +0200

    Example of how to break a object.ForEach() loop

---
+++ Console/BreakForEachObject/Models/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BreakForEachObject.Models
+{
+	internal class Person
+	{
+        #region Properties
+
+        /// <summary>
+        /// Name of person
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Age of person
+        /// </summary>
+		public int Age { get; set; }
+
+		#endregion // Properties
+
+		#region Constructors
+
+		/// <summary>
+		/// Default-Constructor
+		/// </summary>
+		public Person()
+        {
+        
+        }
+
+        /// <summary>
+        /// Full-Constructor
+        /// </summary>
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+		#endregion // Constructors
+
+		#region Functions
+
+        public void PrintInfo()
+        {
+			Console.WriteLine($"\n------------------------");
+            Console.WriteLine($"Name: \"{Name}\"");
+			Console.WriteLine($"Age: \"{Age}\"");
+			Console.WriteLine($"------------------------\n");
+		}
+
+		#endregion // Functions
+
+	}
+}
---
+++ Console/BreakForEachObject/Program.cs
@@ -0,0 +1,59 @@
+﻿using BreakForEachObject.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BreakForEachObject
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Initialize people
+			List<Person> people = new List<Person>()
+			{
+				new Person("Tom", 22),
+				new Person("Michelle", 21),
+				new Person("Peter", 32),
+				new Person("Victoria", 30),
+			};
+
+			// Print person, but abort foreach loop if person older than 30 is recognized
+			FindOldPerson(people);
+
+			Console.WriteLine("\n\nContinue with any key...");
+			Console.ReadKey();
+		}
+
+		/// <summary>
+		/// Print person, but abort foreach loop if person older than 30 is recognized
+		/// </summary>
+		/// <param name="people"></param>
+		private static void FindOldPerson(List<Person> people)
+		{
+			bool oldPersonFound = false;
+			people?.ForEach(p =>
+			{
+				if (p.Age < 30)
+				{
+					p.PrintInfo();
+				}
+				else
+				{
+					// Foreach-Object can be leaved with the return value
+					oldPersonFound = true;
+					return;
+				}
+			});
+
+			if (oldPersonFound)
+			{
+                Console.WriteLine("Old person was detected!");
+            }
+			else
+			{
+                Console.WriteLine("No old person was detected");
+            }
+        }
+
+	}
+}
